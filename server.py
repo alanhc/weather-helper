d1b53b1795e3b68d98ea244ea1079bf3e0c541f9
Alan Tseng <alantseng@AlandeMac-mini.local>
2020-07-23 10:40:04 +0800

    add server

---
+++ server.py
@@ -0,0 +1,26 @@
+from flask import Flask, request
+from flask import jsonify
+import pandas as pd
+
+app = Flask(__name__)
+"""
+curl -X POST -H "Content-Type: application/json" -d '{"tmp" : 30, "h" : "50", "t":"test"}' "http://127.0.0.1:6000/post_json"
+"""
+@app.route("/post_json", methods=['POST'])
+def post_json():
+    r = request.get_json()
+    
+    try:
+        data = [r["t"],r["tmp"],r["h"]]
+        
+        df = pd.DataFrame([data])
+        df.to_csv('data.csv', mode='a+', header=False, index=None)
+    except:
+        return 'error'
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.debug = True
+    #app.run()
+    app.run(host='0.0.0.0', port=6000)
+    
